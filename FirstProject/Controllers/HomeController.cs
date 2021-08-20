21f43bcfb9e1e108f9155d46c38f0e42de28d4fa
Md. Emam Uddin Shahed <48101787+shahed73iu@users.noreply.github.com>
2021-08-20 14:47:28 +0600

    first project

--- FirstProject/Controllers/HomeController.cs
+++ FirstProject/Controllers/HomeController.cs
@@ -11,9 +11,11 @@ namespace FirstProject.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello World";
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good Morning Bangladesh!" : "Good Afternoon Bangladesh";
+            return View("Myview" ,viewModel);
         }
     }
 }
