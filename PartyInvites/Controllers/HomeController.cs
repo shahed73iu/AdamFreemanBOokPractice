636560cfba87b54c5d1e14be2e9fe10e06099036
Md. Emam Uddin Shahed <48101787+shahed73iu@users.noreply.github.com>
2021-08-23 11:52:10 +0600

    before adding Style sheets

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -32,8 +32,21 @@ namespace PartyInvites.Controllers
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
             //Todo: Store response from guest
-            Repository.AddResponse(guestResponse);
-            return View("Thanks", guestResponse);
+            if(ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }    
+          
+        }
+
+        public  ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,14 @@ namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage ="Please enter your Name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your Email")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your Phone number")]
         public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+
         public bool? WillAttend { get; set; }
     }
 }
