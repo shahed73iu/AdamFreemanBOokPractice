c3123bd12262f01622a3bcca7a6df452c034ae31
Md. Emam Uddin Shahed <shahedimamuddin@gmail.com>
2022-02-27 23:20:19 +0600

    SeedData

---
+++ SportsSln/SportsStore/Models/EFStoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFStoreRepository : IStoreRepository
+    {
+        private StoreDbContext _context;
+        public EFStoreRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Product> Products => _context.Products;
+    }
+}
